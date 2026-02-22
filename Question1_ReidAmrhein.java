eac7b83cace680953d83a90db17f17b8a2d0897c
Reid Amrhein <reidamrhein@gmail.com>
2026-02-21 23:24:17 -0500

    Added student pledge to each file

--- Question1_ReidAmrhein.java
+++ Question1_ReidAmrhein.java
@@ -1,1 +1,30 @@
-// testing clone
+/*************************************************************************
+* Assignment 3 for CSCI 271-001 Spring 2026
+*
+* Author: Reid Amrhein
+* OS: Mac
+* Compiler: javac 25.0.1
+* Date: February 21, 2026
+*
+* Purpose
+* We are using recursion to simplify and solve problems
+*
+*************************************************************************/
+
+/*******************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
+*
+* <Reid Amrhein>
+* Said not to include Student ID, so I didn't include it
+********************************************************************/
